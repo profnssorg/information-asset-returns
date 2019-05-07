0f1bd5d89b0295ea2996a0769a33d1c7b8e4f899
bernardopaulsen <bernardohpaulsen@gmail.com>
2019-05-07 17:00:15 -0300

    spiders main / cupom_garch

---
+++ uol_main.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+os.system('open -a Docker')
+
+time.sleep(60)
+
+os.system('docker pull scrapinghub/splash')
+
+time.sleep(30)
+
+os.system('docker run -it -p 8050:8050 scrapinghub/splash --max-timeout 6000')
+
+time.sleep(15)
+
+os.system('scrapy crawl uol -o uol.json')
